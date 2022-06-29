a6cbacff84071bd20423b1dafbd77d2636043634
Vadim <support@zavodil.ru>
2022-06-29 22:47:57 +0300

    staking pool with the complex structure of reward receivers

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,26 +1,18 @@
-use near_contract_standards::fungible_token::core_impl::ext_fungible_token;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::json_types::{U128, U64};
+use near_sdk::json_types::{U128};
 use near_sdk::serde::{Deserialize, Serialize};
 use near_sdk::{
-    env, ext_contract, is_promise_success, log, near_bindgen, require, serde_json, AccountId,
-    Balance, Duration, Gas, PanicOnDefault, Promise, PromiseError, Timestamp, ONE_YOCTO,
+    env, ext_contract, is_promise_success, log, near_bindgen, require, AccountId,
+    Balance, Gas, PanicOnDefault, Promise, Timestamp, EpochHeight
 };
 
-const NO_DEPOSIT: Balance = 0;
 const STAKING_POOL_PING_GAS: Gas = Gas(50_000_000_000_000);
 const STAKING_POOL_READ_GAS: Gas = Gas(5_000_000_000_000);
 const ON_DISTRIBUTE_GAS: Gas = Gas(120_000_000_000_000);
 const WITHDRAW_GAS: Gas = Gas(25_000_000_000_000);
 const ON_WITHDRAW_GAS: Gas = Gas(60_000_000_000_000);
 const UNSTAKE_ALL_GAS: Gas = Gas(50_000_000_000_000);
-const USN_BUY_GAS: Gas = Gas(120_000_000_000_000);
-const ON_BUY_GAS: Gas = Gas(100_000_000_000_000);
-const FT_BALANCE_OF_GAS: Gas = Gas(10_000_000_000_000);
-const FT_TRANSFER_CALL_ADD_FARM_GAS: Gas = Gas(80_000_000_000_000);
-
-const DEFAULT_FARM_DURATION: Duration = 7 * 24 * 60 * 60 * 1_000_000_000;
-const FULL_REWARDS_DURATION: u64 = 3 * 24 * 60 * 60 * 1_000_000_000;
+const NUM_EPOCHS_TO_UNLOCK: EpochHeight = 4;
 
 /// Represents an account structure readable by humans.
 #[derive(Deserialize)]
@@ -48,22 +40,12 @@ pub trait StakingPoolContract {
     fn withdraw(&mut self, amount: U128);
 }
 
-/// Interface for a staking contract
-#[ext_contract(ext_usn)]
-pub trait UsnContract {
-    fn buy(&mut self, expected: Option<String>);
-}
-
 #[ext_contract(ext_self)]
 pub trait ExtContract {
     /* Callback from checking unstaked balance */
     fn on_get_account(&mut self, #[callback] account: StakingPoolAccount);
     /* Callback from staking rewards withdraw */
     fn on_withdraw(&mut self, unstaked_amount: U128, unstake_all: bool);
-    /* Callback from USN buy */
-    fn on_buy(&mut self, #[callback_result] usn_amount: Result<U128, PromiseError>, reward: U128);
-    /* Callback from USN token balance */
-    fn on_usn_balance(&mut self, #[callback] usn_amount: U128);
 }
 
 #[near_bindgen]
@@ -72,20 +54,47 @@ pub trait ExtContract {
 pub struct Contract {
     staking_pool_account_id: AccountId,
     owner_id: AccountId,
-    usn_contract_id: AccountId,
+    reward_receivers: Vec<(AccountId, RewardFeeFraction)>,
+    next_distribution_epoch: EpochHeight,
     #[serde(with = "u128_dec_format")]
     rewards_received: Balance,
-    #[serde(with = "u128_dec_format")]
-    available_rewards: Balance,
     #[serde(with = "u64_dec_format")]
     last_reward_distribution: Timestamp,
-    #[serde(with = "u64_dec_format")]
-    farm_duration: Duration,
-    #[serde(with = "u64_dec_format")]
-    full_rewards_duration: Duration,
-    farm_id: u64,
-    #[serde(with = "u128_dec_format")]
-    usn_distributed: Balance,
+}
+
+#[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Clone)]
+#[serde(crate = "near_sdk::serde")]
+pub struct RewardFeeFraction {
+    pub numerator: u32,
+    pub denominator: u32,
+}
+
+impl RewardFeeFraction {
+    pub fn assert_valid(&self) {
+        assert_ne!(self.denominator, 0, "Denominator must be a positive number");
+        assert!(
+            self.numerator <= self.denominator,
+            "The reward fee must be less or equal to 1"
+        );
+    }
+
+    pub fn zero_fee() -> RewardFeeFraction {
+        RewardFeeFraction {
+            numerator: 0,
+            denominator: 1
+        }
+    }
+
+    pub fn multiply(&self, value: Balance) -> Balance {
+        (U256::from(self.numerator) * U256::from(value) / U256::from(self.denominator)).as_u128()
+    }
+
+    pub fn add(&self, other: RewardFeeFraction) -> RewardFeeFraction {
+        RewardFeeFraction {
+            numerator: (self.numerator * other.denominator + other.numerator * self.denominator),
+            denominator: (self.denominator * other.denominator)
+        }
+    }
 }
 
 #[near_bindgen]
@@ -94,84 +103,52 @@ impl Contract {
     pub fn new(
         staking_pool_account_id: AccountId,
         owner_id: AccountId,
-        usn_contract_id: AccountId,
-        farm_id: u64,
+        reward_receivers: Vec<(AccountId, RewardFeeFraction)>,
     ) -> Self {
+
+        assert_reward_receivers(&reward_receivers);
+
         Self {
             staking_pool_account_id,
             owner_id,
-            usn_contract_id,
+            reward_receivers,
+            next_distribution_epoch: env::epoch_height() + NUM_EPOCHS_TO_UNLOCK,
             rewards_received: 0,
-            available_rewards: 0,
             last_reward_distribution: 0,
-            farm_duration: DEFAULT_FARM_DURATION,
-            full_rewards_duration: FULL_REWARDS_DURATION,
-            farm_id,
-            usn_distributed: 0,
         }
     }
 
-    // #[private]
-    // #[init(ignore_state)]
-    // pub fn migrate() -> Self {
-    //     #[derive(BorshDeserialize)]
-    //     pub struct OldContract {
-    //         staking_pool_account_id: AccountId,
-    //         owner_id: AccountId,
-    //         usn_contract_id: AccountId,
-    //         rewards_received: Balance,
-    //         available_rewards: Balance,
-    //         last_reward_distribution: Timestamp,
-    //         farm_duration: Duration,
-    //         full_rewards_duration: Duration,
-    //         farm_id: u64,
-    //     }
-    //     let OldContract {
-    //         staking_pool_account_id,
-    //         owner_id,
-    //         usn_contract_id,
-    //         rewards_received,
-    //         available_rewards,
-    //         last_reward_distribution,
-    //         farm_duration,
-    //         full_rewards_duration,
-    //         farm_id,
-    //     } = env::state_read().unwrap();
-    //     Self {
-    //         staking_pool_account_id,
-    //         owner_id,
-    //         usn_contract_id,
-    //         rewards_received,
-    //         available_rewards,
-    //         last_reward_distribution,
-    //         farm_duration,
-    //         full_rewards_duration,
-    //         farm_id,
-    //         usn_distributed: 0,
-    //     }
-    // }
+    pub fn reset_reward_receivers(&mut self, reward_receivers: Vec<(AccountId, RewardFeeFraction)>) {
+        self.assert_owner();
+        assert_reward_receivers(&reward_receivers);
+
+        self.reward_receivers = reward_receivers;
+    }
 
     pub fn get_info(&self) -> &Self {
         self
     }
 
-    pub fn ping(&mut self) -> Promise {
-        ext_staking_pool::ping(
-            self.staking_pool_account_id.clone(),
-            NO_DEPOSIT,
-            STAKING_POOL_PING_GAS,
+    pub fn get_rewards_received(&self) -> Balance {
+        self.rewards_received
+    }
+
+
+    // public method to distribute rewards
+    pub fn withdraw(&mut self) -> Promise {
+        assert!(self.next_distribution_epoch <= env::epoch_height(), "The unstaked balance is not yet available due to unstaking delay");
+
+        ext_staking_pool::ext(self.staking_pool_account_id.clone())
+            .with_static_gas(STAKING_POOL_PING_GAS)
+            .ping()
+        .then(ext_staking_pool::ext(self.staking_pool_account_id.clone())
+            .with_static_gas(STAKING_POOL_READ_GAS)
+            .get_account(env::current_account_id())
         )
-        .then(ext_staking_pool::get_account(
-            env::current_account_id(),
-            self.staking_pool_account_id.clone(),
-            NO_DEPOSIT,
-            STAKING_POOL_READ_GAS,
-        ))
-        .then(ext_self::on_get_account(
-            env::current_account_id(),
-            NO_DEPOSIT,
-            ON_DISTRIBUTE_GAS,
-        ))
+        .then(ext_self::ext(env::current_account_id())
+            .with_static_gas(ON_DISTRIBUTE_GAS)
+            .on_get_account()
+        )
     }
 
     #[private]
@@ -183,19 +160,14 @@ impl Contract {
                     "Withdrawing from staking pool: {}",
                     account.unstaked_balance.0
                 );
-                ext_staking_pool::withdraw(
-                    account.unstaked_balance,
-                    self.staking_pool_account_id.clone(),
-                    NO_DEPOSIT,
-                    WITHDRAW_GAS,
+                ext_staking_pool::ext(self.staking_pool_account_id.clone())
+                    .with_static_gas(WITHDRAW_GAS)
+                    .withdraw(account.unstaked_balance)
+                .then(
+                    ext_self::ext(env::current_account_id())
+                        .with_static_gas(ON_WITHDRAW_GAS)
+                        .on_withdraw(account.unstaked_balance, unstake_all)
                 )
-                .then(ext_self::on_withdraw(
-                    account.unstaked_balance,
-                    unstake_all,
-                    env::current_account_id(),
-                    NO_DEPOSIT,
-                    ON_WITHDRAW_GAS,
-                ))
                 .as_return();
             } else {
                 log!("Awaiting unstaking. Nothing to do. Can't withdraw yet");
@@ -206,12 +178,10 @@ impl Contract {
     }
 
     fn internal_unstake_all(&mut self) {
-        log!("Unstaking all from staking pool",);
-        ext_staking_pool::unstake_all(
-            self.staking_pool_account_id.clone(),
-            NO_DEPOSIT,
-            UNSTAKE_ALL_GAS,
-        )
+        log!("Unstaking all from staking pool");
+        ext_staking_pool::ext(self.staking_pool_account_id.clone())
+            .with_static_gas(UNSTAKE_ALL_GAS)
+            .unstake_all()
         .as_return();
     }
 
@@ -223,129 +193,34 @@ impl Contract {
             unstaked_amount.0
         );
         self.rewards_received += unstaked_amount.0;
-        // TODO: Send some rewards to the owner.
-        self.available_rewards += unstaked_amount.0;
+
+        // Send rewards
+        for reward_receiver in &self.reward_receivers {
+            transfer(reward_receiver.0.clone(), reward_receiver.1.multiply(unstaked_amount.0));
+        }
+
         if unstake_all {
             self.internal_unstake_all();
         }
     }
 
-    pub fn set_full_rewards_duration(&mut self, full_rewards_duration_sec: u32) {
-        self.assert_owner();
-        self.full_rewards_duration = u64::from(full_rewards_duration_sec) * 10u64.pow(9);
-    }
-
-    pub fn set_farm_duration(&mut self, farm_duration_sec: u32) {
-        self.assert_owner();
-        self.farm_duration = u64::from(farm_duration_sec) * 10u64.pow(9);
-    }
-
-    pub fn get_near_reward_for_distribution(&self) -> U128 {
-        let time_diff = env::block_timestamp() - self.last_reward_distribution;
-        if time_diff >= self.full_rewards_duration {
-            self.available_rewards
-        } else {
-            u128_ratio(
-                self.available_rewards,
-                time_diff,
-                self.full_rewards_duration,
-            )
-        }
-        .into()
-    }
-
     #[payable]
     pub fn donate(&mut self) {
-        let attached_deposit = env::attached_deposit();
-        log!("Thank for you {} NEAR", attached_deposit);
-        self.rewards_received += attached_deposit;
-        self.available_rewards += attached_deposit;
+        log!("Thank you for your {} yNEAR", env::attached_deposit())
+    }
+    pub fn get_current_env_data(&self) -> (u64, u64) {
+        let now = env::block_timestamp();
+        let eh = env::epoch_height();
+        (now, eh)
     }
 
-    pub fn distribute_near(&mut self) -> Promise {
-        let reward = self.get_near_reward_for_distribution().0;
-        require!(reward > 0, "Nothing to distribute");
-
-        self.available_rewards -= reward;
-        self.last_reward_distribution = env::block_timestamp();
-
-        ext_usn::buy(None, self.usn_contract_id.clone(), reward, USN_BUY_GAS).then(
-            ext_self::on_buy(
-                U128(reward),
-                env::current_account_id(),
-                NO_DEPOSIT,
-                ON_BUY_GAS,
-            ),
-        )
-    }
-
-    #[private]
-    pub fn on_buy(
-        &mut self,
-        #[callback_result] usn_amount: Result<U128, PromiseError>,
-        reward: U128,
-    ) {
-        if let Ok(usn_amount) = usn_amount {
-            self.internal_distribute_usn(usn_amount.0).as_return();
-        } else {
-            log!("Failed to buy USN");
-            self.available_rewards += reward.0;
-        }
-    }
-
-    #[private]
-    pub fn on_usn_balance(&mut self, #[callback] usn_amount: U128) {
-        if usn_amount.0 > 0 {
-            self.internal_distribute_usn(usn_amount.0).as_return();
-        }
-    }
-
-    pub fn distribute_usn(&mut self) -> Promise {
-        ext_fungible_token::ft_balance_of(
-            env::current_account_id(),
-            self.usn_contract_id.clone(),
-            NO_DEPOSIT,
-            FT_BALANCE_OF_GAS,
-        )
-        .then(ext_self::on_usn_balance(
-            env::current_account_id(),
-            NO_DEPOSIT,
-            ON_BUY_GAS,
-        ))
-    }
 
     pub fn get_staking_pool(&self) -> AccountId {
         self.staking_pool_account_id.clone()
     }
 }
 
-#[derive(Serialize)]
-#[serde(crate = "near_sdk::serde")]
-pub struct FarmingDetails {
-    /// End date of the farm.
-    pub end_date: U64,
-    /// Existing farm ID.
-    pub farm_id: u64,
-}
-
 impl Contract {
-    pub fn internal_distribute_usn(&mut self, usn_amount: Balance) -> Promise {
-        self.usn_distributed += usn_amount;
-        ext_fungible_token::ft_transfer_call(
-            self.staking_pool_account_id.clone(),
-            usn_amount.into(),
-            Some(format!("Enjoy reward of {} USN, friends", usn_amount)),
-            serde_json::to_string(&FarmingDetails {
-                end_date: U64::from(env::block_timestamp() + self.farm_duration),
-                farm_id: self.farm_id,
-            })
-            .unwrap(),
-            self.usn_contract_id.clone(),
-            ONE_YOCTO,
-            FT_TRANSFER_CALL_ADD_FARM_GAS,
-        )
-    }
-
     pub fn assert_owner(&self) {
         assert_eq!(
             &self.owner_id,
@@ -358,10 +233,6 @@ impl Contract {
 uint::construct_uint!(
     pub struct U256(4);
 );
-
-pub(crate) fn u128_ratio(a: u128, num: u64, denom: u64) -> Balance {
-    (U256::from(a) * U256::from(num) / U256::from(denom)).as_u128()
-}
 
 pub(crate) mod u128_dec_format {
     use near_sdk::serde::de;
@@ -404,5 +275,22 @@ pub(crate) mod u64_dec_format {
         String::deserialize(deserializer)?
             .parse()
             .map_err(de::Error::custom)
+    }
+}
+
+fn assert_reward_receivers (reward_receivers: &Vec<(AccountId, RewardFeeFraction)>){
+    let mut total_fee: RewardFeeFraction = RewardFeeFraction::zero_fee();
+    for reward_receiver in reward_receivers {
+        reward_receiver.1.assert_valid();
+        total_fee = total_fee.add(reward_receiver.1.clone());
+    }
+    total_fee.assert_valid();
+    assert_eq!(total_fee.numerator, total_fee.denominator, "ERR_ILLEGAL_REWARD_RECEIVERS");
+}
+
+fn transfer(account: AccountId, amount: Balance) {
+    if amount > 0 {
+        log!("Sending {} to {}", amount, account);
+        Promise::new(account).transfer(amount);
     }
 }
---
+++ contract/tests/workspaces/staking.rs
@@ -0,0 +1,228 @@
+use near_sdk::{Balance};
+use serde_json::json;
+use near_units::{parse_gas, parse_near};
+use near_sdk::serde::{Deserialize, Serialize};
+use workspaces::{Account, Worker};
+use workspaces::network::Sandbox;
+
+use workspaces::prelude::*;
+
+const STAKING_KEY: &str = "KuTCtARNzxZQ3YvXDeLjx83FDqxv2SdQTSbiq876zR7";
+const REWARD_1_ACCOUNT: &str = "reward_1";
+const REWARD_1_FEE: u128 = 10;
+const REWARD_2_ACCOUNT: &str = "reward_2";
+const REWARD_2_FEE: u128 = 90;
+
+const CONTRACT_WASM_FILEPATH: &str = "./../out/main.wasm";
+const POOL_WASM_FILEPATH: &str = "./../out/staking_pool.wasm";
+
+// wait ~ 4+ epoch
+const DELTA_HEIGHT: u64 = 2100;
+
+#[derive(Serialize, Deserialize, Clone)]
+#[serde(crate = "near_sdk::serde")]
+pub struct RewardFeeFraction {
+    pub numerator: u32,
+    pub denominator: u32,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let worker: Worker<Sandbox> = workspaces::sandbox().await?;
+    let owner = worker.root_account();
+    let alice: Account = owner.create_subaccount(&worker, "alice")
+        .initial_balance(parse_near!("11 N"))
+        .transact()
+        .await?
+        .into_result()?;
+
+    let reward_1: Account = owner.create_subaccount(&worker, REWARD_1_ACCOUNT)
+        .transact()
+        .await?
+        .into_result()?;
+
+    let reward_2: Account = owner.create_subaccount(&worker, REWARD_2_ACCOUNT)
+        .transact()
+        .await?
+        .into_result()?;
+
+    let init_balance_1 = reward_1.view_account(&worker).await?.balance;
+    let init_balance_2 = reward_2.view_account(&worker).await?.balance;
+
+    println!("alice account: {}, reward_1: {}, reward_2: {}", alice.id(), reward_1.id(), reward_2.id());
+
+    let contract_wasm = std::fs::read(CONTRACT_WASM_FILEPATH)?;
+    let contract = worker.dev_deploy(&contract_wasm).await?;
+
+    let pool_wasm = std::fs::read(POOL_WASM_FILEPATH)?;
+    let pool = worker.dev_deploy(&pool_wasm).await?;
+
+    println!("Pool Account ID: {}", pool.id());
+    println!("Owner Account ID: {}", contract.id());
+
+    let _outcome_init_owner = contract
+        .call(&worker, "new")
+        .args_json(json!({
+                "staking_pool_account_id": pool.id(),
+                "owner_id": owner.id(),
+                "reward_receivers": [
+                    [reward_1.id(),
+                        {"numerator": REWARD_1_FEE as u32, "denominator": 100u32}],
+                    [reward_2.id(),
+                        {"numerator": REWARD_2_FEE as u32, "denominator": 100u32}]
+                ]
+        }))?
+        .gas(parse_gas!("300 T") as u64)
+        .transact()
+        .await?;
+    //println!("init owner contract outcome: {:#?}", _outcome_init_owner);
+
+    let _outcome_donate_rewards = owner
+        .call(&worker, contract.id(), "donate")
+        .deposit(parse_near!("100 N"))
+        .transact()
+        .await?;
+    // println!("donate outcome: {:#?}", _outcome_donate_rewards);
+
+    let _outcome_init_pool = pool
+        .call(&worker, "new")
+        .args_json(json!({
+                "owner_id": contract.id(),
+                "stake_public_key": STAKING_KEY,
+                "reward_fee_fraction": {
+                    "numerator": 10u32,
+                    "denominator": 100u32
+                }
+        }))?
+        .gas(parse_gas!("300 T") as u64)
+        .transact()
+        .await?;
+    //println!("init pool outcome: {:#?}", _outcome_init_pool);
+
+    let _outcome_deposit_and_stake = alice
+        .call(&worker, pool.id(), "deposit_and_stake")
+        .gas(parse_gas!("50 T") as u64)
+        .deposit(parse_near!("10 N"))
+        .transact()
+        .await?;
+    // println!("deposit_and_stake outcome: {:#?}", _outcome_deposit_and_stake);
+
+    let _outcome_account: serde_json::Value = pool.call(&worker, "get_account")
+        .args_json(json!({
+                "account_id": alice.id(),
+        }))?
+        .view()
+        .await?
+        .json()?;
+    //println!("account outcome: {:#?}", _outcome_account);
+
+    let (_timestamp, epoch_height_1): (u64, u64) = contract
+        .call(&worker, "get_current_env_data")
+        .view()
+        .await?
+        .json()?;
+    //println!("timestamp = {}, epoch_height = {}", _timestamp, epoch_height_1);
+
+    //let block_info = worker.view_latest_block().await?;
+    //println!("BlockInfo pre-fast_forward {:?}", block_info);
+
+    worker.fast_forward(DELTA_HEIGHT).await?;
+
+    let (_timestamp, epoch_height_2): (u64, u64) = contract
+        .call(&worker, "get_current_env_data")
+        .view()
+        .await?
+        .json()?;
+    //println!("timestamp = {}, epoch_height = {}", _timestamp, epoch_height_2);
+
+    println!("===> {} epochs passed", (epoch_height_2 - epoch_height_1).to_string());
+
+    //let block_info = worker.view_latest_block().await?;
+    //println!("BlockInfo post-fast_forward {:?}", block_info);
+
+    let _outcome_account: serde_json::Value = pool.call(&worker, "get_account")
+        .args_json(json!({
+                "account_id": alice.id(),
+        }))?
+        .view()
+        .await?
+        .json()?;
+    //println!("account outcome: {:#?}", outcome_account);
+
+    let _outcome_withdraw = owner
+        .call(&worker, contract.id(), "withdraw")
+        .gas(parse_gas!("200 T") as u64)
+        .transact()
+        .await?;
+    //println!("withdraw outcome: {:#?}", _outcome_withdraw);
+
+    let (_timestamp, epoch_height_3): (u64, u64) = contract
+        .call(&worker, "get_current_env_data")
+        .view()
+        .await?
+        .json()?;
+    //println!("timestamp = {}, epoch_height = {}", _timestamp, epoch_height_3);
+
+    //let block_info = worker.view_latest_block().await?;
+    //println!("BlockInfo pre-fast_forward {:?}", block_info);
+
+    // wait ~ 5 epoch
+    worker.fast_forward(DELTA_HEIGHT).await?;
+
+    let (_timestamp, epoch_height_4): (u64, u64) = contract
+        .call(&worker, "get_current_env_data")
+        .view()
+        .await?
+        .json()?;
+    //println!("timestamp = {}, epoch_height = {}", _timestamp, epoch_height_4);
+
+    println!("===> {} epochs passed", (epoch_height_4 - epoch_height_3).to_string());
+
+    //let block_info = worker.view_latest_block().await?;
+    //println!("BlockInfo post-fast_forward {:?}", block_info);
+
+    let _outcome_account: serde_json::Value = pool.call(&worker, "get_account")
+        .args_json(json!({
+                "account_id": alice.id(),
+        }))?
+        .view()
+        .await?
+        .json()?;
+    // println!("account outcome: {:#?}", _outcome_account);
+
+    let _outcome_withdraw = owner
+        .call(&worker, contract.id(), "withdraw")
+        .gas(parse_gas!("200 T") as u64)
+        .transact()
+        .await?;
+    println!("withdraw outcome: {:#?}", _outcome_withdraw);
+
+    let rewards_received: Balance = contract.call(&worker, "get_rewards_received")
+        .view()
+        .await?
+        .json::<Balance>()?;
+    println!("rewards_received: {}", rewards_received);
+
+    println!("1: {}", reward_1.view_account(&worker).await?.balance);
+    println!("1: {}", rewards_received / 100 * REWARD_1_FEE);
+
+    println!("2: {}", reward_2.view_account(&worker).await?.balance);
+    println!("2: {}", rewards_received / 100 * REWARD_2_FEE);
+
+    almost_eq(reward_1.view_account(&worker).await?.balance - init_balance_1, rewards_received / 100 * REWARD_1_FEE, 3);
+    almost_eq(reward_2.view_account(&worker).await?.balance - init_balance_2, rewards_received / 100 * REWARD_2_FEE, 3);
+
+    Ok(())
+}
+
+pub fn almost_eq(a: u128, b: u128, prec: u32) {
+    let p = 10u128.pow(23 - prec);
+    let ap = (a + p / 2) / p;
+    let bp = (b + p / 2) / p;
+    assert_eq!(
+        ap,
+        bp,
+        "{}",
+        format!("Expected {} to eq {}, with precision {}", a, b, prec)
+    );
+}
